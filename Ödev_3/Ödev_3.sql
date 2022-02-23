11d825aeaa406d2d693d6283f01a33e33e616ab3
ibrahimbayramli <54009509+ibrahimbayramli@users.noreply.github.com>
2022-02-23 22:14:56 +0300

    Add files via upload

---
+++ Ödev_3/Ödev_3.sql
@@ -0,0 +1,11 @@
+-- 1. Soru
+--SELECT country FROM country WHERE country LIKE 'A%a';
+
+-- 2. Soru
+--SELECT country FROM country WHERE country LIKE '_____%n';
+
+-- 3. Soru
+--SELECT title FROM film WHERE title ILIKE '%T%T%T%T%';
+
+-- 4. Soru
+--SELECT * FROM film WHERE (title LIKE 'C%') AND (length>90) AND (rental_rate=2.99);
