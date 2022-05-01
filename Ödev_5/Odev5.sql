71ca0eb6daa2863b4237287409eac89c4f89d378
ibrahimbayramli <54009509+ibrahimbayramli@users.noreply.github.com>
2022-05-01 04:52:53 +0300

    Create Odev5.sql

---
+++ Ödev_5/Odev5.sql
@@ -0,0 +1,16 @@
+-- 1. Soru
+SELECT * FROM film 
+WHERE title LIKE '%n' ORDER BY length DESC
+LIMIT 5
+
+-- 2. Soru
+SELECT * FROM film 
+WHERE title LIKE '%n' ORDER BY length 
+OFFSET 5
+LIMIT 5
+
+-- 3. Soru
+SELECT * FROM customer 
+WHERE customer.store_id=1
+ORDER BY customer.last_name DESC
+LIMIT 4
